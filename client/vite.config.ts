cdc61dcc7baae9327f4b737a433f04b1b8fb51fe
Artur Agh <aghartur@gmail.com>
2025-07-04 23:38:16 +0200

    vite shadcn setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+    },
 })
