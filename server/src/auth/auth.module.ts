4b9274181b71869bc156efdf78fbc4e5b8950c36
Artur Agh <aghartur@gmail.com>
2025-07-04 16:39:59 +0200

    create tokens

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { getJwtConfig } from '@/config/jwt.config'
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+	imports: [JwtModule.registerAsync({
+		imports: [ConfigModule],
+		inject: [ConfigService],
+		useFactory: getJwtConfig
+
+	})],
+	controllers: [AuthController],
+	providers: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule {
+}
