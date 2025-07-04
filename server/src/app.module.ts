d62ebe55bda48da322af2b53253cc00453512b9e
Artur Agh <aghartur@gmail.com>
2025-07-04 14:13:21 +0200

    chore: cleaned dummy git errors and adjusted .gitignore

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from "@nestjs/config";
+import {IS_DEV_ENV} from "./lib/utils/is-dev.util";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot({
+        ignoreEnvFile: !IS_DEV_ENV,
+        isGlobal: true
+    }),],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,30 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ConfigService} from "@nestjs/config";
+import * as cookieParser from 'cookie-parser';
+import {ValidationPipe} from "@nestjs/common";
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    const config = app.get(ConfigService)
+
+    app.use(cookieParser());
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true
+        })
+    )
+
+    app.enableCors({
+        origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+        credentials: true,
+        exposeHeaders: ['set-cookie']
+    })
+
+
+    await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
