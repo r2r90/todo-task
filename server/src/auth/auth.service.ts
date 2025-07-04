30c33419c5a55029eb9b0d66a4c60aca0259b264
Artur Agh <aghartur@gmail.com>
2025-07-04 16:39:52 +0200

    create tokens

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,10 +1,26 @@
 import { ConflictException, Injectable } from '@nestjs/common'
 import { PrismaService } from '@/prisma/prisma.service'
 import { RegisterDto } from '@/auth/dto/register.dto'
+import { hash } from 'argon2'
+import { ConfigService } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
+import { JwtPayload } from '@/auth/interfaces/jwt.interface'
 
 @Injectable()
 export class AuthService {
-	constructor(private readonly prismaService: PrismaService) {
+
+	private readonly JWT_SECRET: string
+	private readonly JWT_ACCESS_TOKEN_TTL: string
+	private readonly JWT_REFRESH_TOKEN_TTL: string
+
+	constructor(
+		private readonly prismaService: PrismaService,
+		private readonly configService: ConfigService,
+		private readonly jwtService: JwtService
+	) {
+		this.JWT_SECRET = configService.getOrThrow<string>('JWT_SECRET')
+		this.JWT_ACCESS_TOKEN_TTL = configService.getOrThrow<string>('JWT_ACCESS_TOKEN_TTL')
+		this.JWT_REFRESH_TOKEN_TTL = configService.getOrThrow<string>('JWT_REFRESH_TOKEN_TTL')
 	}
 
 	async register(dto: RegisterDto) {
@@ -20,10 +36,30 @@ export class AuthService {
 
 		const user = await this.prismaService.user.create({
 			data: {
-				firstName, lastName, email, password
+				firstName,
+				lastName,
+				email,
+				password: await hash(password)
 			}
 		})
 
-		return user
+		return this.generateTokens(user.id)
 	}
+
+	private generateTokens(id: string) {
+		const payload: JwtPayload = { id }
+
+		const accessToken = this.jwtService.sign(payload, {
+			expiresIn: this.JWT_ACCESS_TOKEN_TTL
+		})
+
+		const refreshToken = this.jwtService.sign(payload, {
+			expiresIn: this.JWT_REFRESH_TOKEN_TTL
+		})
+
+		return { accessToken, refreshToken }
+
+	}
+
+
 }
